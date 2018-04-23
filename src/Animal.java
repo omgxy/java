d9546a583eb1b0e908b3f9fdaa39249c016c3cbe
lijun4 <lijun4@unionpay.com>
2018-04-23 18:57:40 +0800

    animal

---
+++ src/Animal.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author lijun4
+ *
+ */
+public class Animal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
