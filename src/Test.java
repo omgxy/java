92b70ef23d4432fb1e1c710568dfd90b9a422f8d
lijun4 <lijun4@unionpay.com>
2018-04-23 18:56:44 +0800

    test

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author lijun4
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo");
+	}
+
+}
